37abd58f3a437c6c52be2a3231a7f4de55bb45f2
vodis <i40217891@gmail.com>
2024-01-10 22:34:15 +0000

    CSP-1 - Run fork on localhost
    - update hardhat config
    - update .env.example

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,15 +1,26 @@
+require("hardhat-gas-reporter");
+const dotenv = require("dotenv");
+dotenv.config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   defaultNetwork: "hardhat",
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 23,
+    token: 'ETH'
+  },
   networks: {
     hardhat: {
+      chainId: 1337,
       forking: {
-        url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+        url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+        blockNumber: 18888888,        
       },
       accounts: {
-        count: 100,
+        count: 10,
       },
     },
-  }
+  },
 };
